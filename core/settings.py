2351b4b7eb076f6f0d774d4dfa710507e1f33edb
DavidBugger <bwehfukdavid@gmail.com>
2026-01-07 12:14:17 +0100

    chore: rollback to sqlite database

--- core/settings.py
+++ core/settings.py
@@ -75,17 +75,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if env('DATABASE_URL', default=None):
-    DATABASES = {
-        'default': env.db(),
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
